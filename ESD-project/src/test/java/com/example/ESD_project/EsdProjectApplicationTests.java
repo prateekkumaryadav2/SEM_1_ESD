62f1eacf5568c286f6be978542b647460ff40e08
prateekkumaryadav2 <prateek.student20@gmail.com>
2025-11-21 17:48:53 +0530

    initialized spring

---
+++ ESD-project/src/test/java/com/example/ESD_project/EsdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ESD_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
